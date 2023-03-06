405ee7440e6c95e584057b0b022354be7f95c530
João Benício <j.beniciopp@gmail.com>
2023-03-06 18:00:03 -0300

    Componente SearchBar concluido

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,81 +8,47 @@ export default function SearchBar() {
   const { setApi } = useContext(RecipesContext);
   const [searchRadio, setSearchRadio] = useState('');
   const [search, setSearch] = useState('');
+  const doze = 12;
 
   const onClickButton = async () => {
-    console.log(searchRadio);
-    console.log(search);
-
-    const first = 'first-letter';
-
-    if (searchRadio === first && search.length > 1) {
+    if (searchRadio === 'first-letter' && search.length > 1) {
       return global.alert('Your search must have only 1 (one) character');
-    } if (searchRadio === 'ingredient' && window.location.pathname === '/meals') {
-      const link = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`;
-      console.log(link);
-      const api = await searchApi(link);
-      console.log(api.meals);
+    }
+
+    const searchTypeToApiEndpoint = {
+      'first-letter': {
+        meals: 'https://www.themealdb.com/api/json/v1/1/search.php?f=',
+        drinks: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=',
+      },
+      ingredient: {
+        meals: 'https://www.themealdb.com/api/json/v1/1/filter.php?i=',
+        drinks: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=',
+      },
+      name: {
+        meals: 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+        drinks: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
+      },
+    };
+
+    const apiEndpoint = searchTypeToApiEndpoint[searchRadio][window
+      .location.pathname.slice(1)];
+    const link = apiEndpoint + search;
+    console.log(link);
+    const api = await searchApi(link);
+    console.log(api);
+
+    if (api.meals && api.meals.length > 0) {
       if (api.meals.length === 1) {
         history.push(`/meals/${api.meals[0].idMeal}`);
-      } if (api.meals.length === 0) {
-        global.alert("Sorry, we haven't found any recipes for these filters.");
       }
-      setApi(api.meals.slice(0, 12));
-    } if (searchRadio === 'name' && window.location.pathname === '/meals') {
-      const link = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
-      console.log(link);
-      const api = await searchApi(link);
-      console.log(api.meals);
-      if (api.meals.length === 1) {
-        history.push(`/meals/${api.meals[0].idMeal}`);
-      } if (api.meals.length === 0) {
-        global.alert("Sorry, we haven't found any recipes for these filters.");
-      }
-      setApi(api.meals.slice(0, 12));
-    } if (searchRadio === first && window.location.pathname === '/meals') {
-      const link = `https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`;
-      console.log(link);
-      const api = await searchApi(link);
-      console.log(api.meals);
-      if (api.meals.length === 1) {
-        history.push(`/meals/${api.meals[0].idMeal}`);
-      } if (api.meals.length === 0) {
-        global.alert("Sorry, we haven't found any recipes for these filters.");
-      }
-      setApi(api.meals.slice(0, 12));
-    } if (searchRadio === 'ingredient' && window.location.pathname === '/drinks') {
-      const link = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${search}`;
-      console.log(link);
-      const api = await searchApi(link);
-      console.log(api.drinks);
+      setApi(api.meals.slice(0, doze));
+    } else if (api.drinks && api.drinks.length > 0) {
       if (api.drinks.length === 1) {
         history.push(`/drinks/${api.drinks[0].idDrink}`);
-      } if (api.drinks.length === 0) {
-        global.alert("Sorry, we haven't found any recipes for these filters.");
       }
-      setApi(api.drinks.slice(0, 12));
-    } if (searchRadio === 'name' && window.location.pathname === '/drinks') {
-      const link = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
-      console.log(link);
-      const api = await searchApi(link);
-      console.log(api.drinks);
-      if (api.drinks.length === 1) {
-        history.push(`/drinks/${api.drinks[0].idDrink}`);
-      } if (api.drinks.length === 0) {
-        global.alert("Sorry, we haven't found any recipes for these filters.");
-      }
-      setApi(api.drinks.slice(0, 12));
-    } if (searchRadio === first && window.location.pathname === '/drinks') {
-      const link = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${search}`;
-      console.log(link);
-      const api = await searchApi(link);
-      console.log(api.drinks);
-      if (api.drinks.length === 1) {
-        history.push(`/drinks/${api.drinks[0].idDrink}`);
-      } if (api.drinks.length === 0) {
-        global.alert("Sorry, we haven't found any recipes for these filters.");
-      }
-      setApi(api.drinks.slice(0, 12));
+      setApi(api.drinks.slice(0, doze));
+    } else {
+      global.alert('Sorry, we haven\'t found any recipes for these filters.');
     }
   };
 
