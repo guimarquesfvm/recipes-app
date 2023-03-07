0b31edf29a0d9e7f1de052b8bfc97c1dd9ae9056
João Benício <j.beniciopp@gmail.com>
2023-03-07 16:21:30 -0300

    Tela Done Recipes concluida

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,70 +1,77 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import share from '../images/shareIcon.svg';
 
 export default function DoneRecipes() {
-  useEffect(() => {
-    const obj = [
-      {
-        alcoholicOrNot: '',
-        category: 'Vegetarian',
-        doneDate: '23/06/2020',
-        id: '52771',
-        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-        name: 'Spicy Arrabiata Penne',
-        nationality: 'Italian',
-        tags: ['Pasta', 'Curry'],
-        type: 'meal',
-      },
-      {
-        alcoholicOrNot: 'Alcoholic',
-        category: 'Cocktail',
-        doneDate: '23/06/2020',
-        id: '178319',
-        image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-        name: 'Aquamarine',
-        nationality: '',
-        tags: [],
-        type: 'drink',
-      },
-    ];
-
-    const objJSON = JSON.stringify(obj);
-
-    localStorage.setItem('doneRecipes', objJSON);
-  }, []);
+  const [linkCopied, setLinkCopied] = useState(false);
+  const [filterClick, setFilterClick] = useState('All');
 
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
 
-  const doneRecipesMap = () => doneRecipes.map((e, i) => (
-    <div key={ i } href={ `/${e.type}s/${e.id}` }>
-      <img
-        src={ e.image }
-        alt="Foto da Receita"
-        data-testid={ `${i}-horizontal-image` }
-        width="300px"
-      />
-      <p data-testid={ `${i}-horizontal-top-text` }>
-        { `${e.nationality} - ${e.category}` }
-      </p>
-      <p data-testid={ `${i}-horizontal-name` }>{ e.name }</p>
-      <p data-testid={ `${i}-horizontal-done-date` }>{ e.doneDate }</p>
-      <p data-testid={ `${i}-horizontal-top-text` }>{ e.alcoholicOrNot }</p>
-      <button
-        type="button"
-        data-testid={ `${i}-horizontal-share-btn` }
-        src={ share }
-      >
-        Share
-      </button>
-      { e.tags.map((elem, ind) => (
-        <div
-          key={ ind }
-          data-testid={ `0-${elem}-horizontal-tag` }
+  function onClickShare(element) {
+    const copyText = `${window.location.origin}/${element.type}s/${element.id}`;
+
+    navigator.clipboard.writeText(copyText);
+
+    // console.log(copyText);
+
+    return setLinkCopied(true);
+  }
+
+  const filter = () => {
+    if (filterClick === 'All') {
+      return doneRecipes;
+    } if (filterClick === 'Meals') {
+      return doneRecipes.filter((element) => element.type === 'meal');
+    } if (filterClick === 'Drinks') {
+      return doneRecipes.filter((element) => element.type === 'drink');
+    }
+  };
+
+  const doneRecipesMap = () => {
+    const array = filter();
+    const cards = array.map((e, i) => (
+      <div key={ i }>
+        <Link
+          to={ `/${e.type}s/${e.id}` }
         >
-          {elem}
-        </div>)) }
-    </div>
-  ));
+          <img
+            src={ e.image }
+            alt="Foto da Receita"
+            data-testid={ `${i}-horizontal-image` }
+            width="300px"
+          />
+        </Link>
+        <p data-testid={ `${i}-horizontal-top-text` }>
+          { `${e.nationality} - ${e.category}` }
+        </p>
+        <Link
+          to={ `/${e.type}s/${e.id}` }
+          data-testid={ `${i}-horizontal-name` }
+        >
+          { e.name }
+        </Link>
+        <p data-testid={ `${i}-horizontal-done-date` }>{ e.doneDate }</p>
+        <p data-testid={ `${i}-horizontal-top-text` }>{ e.alcoholicOrNot }</p>
+        <button
+          type="button"
+          data-testid={ `${i}-horizontal-share-btn` }
+          src={ share }
+          onClick={ () => onClickShare(e) }
+        >
+          <img src={ share } alt="Share button" />
+        </button>
+        { e.tags.map((elem, ind) => (
+          <div
+            key={ ind }
+            data-testid={ `0-${elem}-horizontal-tag` }
+          >
+            {elem}
+          </div>)) }
+      </div>
+    ));
+    return cards;
+  };
 
   const start = () => {
     if (doneRecipes === null) {
@@ -74,27 +81,37 @@ export default function DoneRecipes() {
     }
   };
 
+  const linkCopiedFunc = () => {
+    if (linkCopied === true) {
+      return <p>Link copied!</p>;
+    }
+  };
+
   return (
     <div>
       <form>
         <button
           type="button"
-          data-testid="filter-by-all-btn"
-        >
-          All
-        </button>
-        <button
-          type="button"
           data-testid="filter-by-meal-btn"
+          onClick={ () => setFilterClick('Meals') }
         >
           Meals
         </button>
         <button
           type="button"
           data-testid="filter-by-drink-btn"
+          onClick={ () => setFilterClick('Drinks') }
         >
           Drinks
         </button>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setFilterClick('All') }
+        >
+          All
+        </button>
+        { linkCopiedFunc() }
         { start() }
       </form>
     </div>
