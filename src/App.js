b7fbd44cdb33db00598dbcf03b22eb2204e4e197
Pietro Bucker <pietro_177@hotmail.com>
2023-03-08 19:01:05 -0300

    em espera

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import RecipesProvider from './context/RecipesProvider';
 import Drinks from './pages/Drinks';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
         <Route path="/meals/:id-da-receita/in-progress" />
         <Route path="/profile" component={ Profile } />
         <Route path="/done-recipes" component={ DoneRecipes } />
-        <Route path="/favorite-recipes" />
+        <Route path="/favorite-recipes" component={ FavoriteRecipes } />
       </Switch>
     </RecipesProvider>
   );
